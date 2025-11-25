17033b99efd720f1d7c233431c92a1a5f99113f0
gurinaviktoria <gurinavika6@gmail.com>
2025-11-25 13:35:52 +0300

    Add GitHub Actions workflow for Flutter Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
+import 'package:flutter/foundation.dart'; // Для kIsWeb
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+// Sqflite
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
 import 'theme_provider.dart';
 import 'routes/app_router.dart';
 import 'viewmodels/home_viewmodel.dart';
 import 'app_themes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Ініціалізація sqflite для Web
+  if (kIsWeb) {
+    databaseFactory = databaseFactoryFfiWeb;
+  }
+
   runApp(
     MultiProvider(
       providers: [
